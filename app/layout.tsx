cbd0921d7e4342f68345c836c6a2ad78fd35008a
Vladyslav Honchar <vladyslav.honchar@tangramltd.com>
2025-08-04 02:25:43 +0300

    feat(layout): add root layout with providers

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import {Geist, Geist_Mono} from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactNode, useState} from "react";
+import {TanStackProvider} from "@/components/TanStackProvider/TanStackProvider";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: ReactNode;
+}>) {
+
+    return (
+        <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <TanStackProvider>
+            <div className="wrapper">
+                <Header/>
+                <main>
+                    <div className="container">
+                        {children}
+                    </div>
+                </main>
+                <Footer/>
+            </div>
+        </TanStackProvider>
+        </body>
+        </html>
+    );
+}
