7996dbb0d641130d4f255879b15a46bfb194f55d
Vladyslav Honchar <vladyslav.honchar@tangramltd.com>
2025-08-04 02:26:14 +0300

    feat(notes): add notes page with SSR

---
+++ app/notes/page.tsx
@@ -0,0 +1,27 @@
+import NotesClient from "@/app/notes/Notes.client";
+import {HydrationBoundary, QueryClient} from "@tanstack/react-query";
+import {getAllNotes} from "@/lib/api";
+import {dehydrate} from "@tanstack/query-core";
+
+interface PageProps {
+
+}
+
+const Page = async ({}: PageProps) => {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+        queryKey: ['notes', '', 1],
+        queryFn: () => getAllNotes('', 1),
+    })
+
+    return (
+        <>
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <NotesClient/>
+            </HydrationBoundary>
+        </>
+    );
+};
+
+export default Page;
